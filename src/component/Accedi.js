270b8f0329d62c767fb5b4d4a709c841071ac2c1
giorgiocoppola <giorgiocoppola12@gmail.com>
2022-09-28 18:52:29 +0200

    Creazione della pagina web di accesso

--- src/component/Accedi.js
+++ src/component/Accedi.js
@@ -1,8 +1,60 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {Link} from 'react-router-dom'
 import './Accedi.css'
 
 function Accedi() {
+
+    const [email,setEmail] = useState("");
+    const [password,setPassword] = useState("");
+    const [message,setMessage] = useState("");
+    const [message2,setMessage2] = useState("");
+    const [message3,setMessage3] = useState("");
+
+    const handleSubmit = (e) => {
+
+        e.preventDefault();
+
+
+    if(email === "" && password === "") {
+
+            
+ 
+        setMessage("Campo obbligatorio");
+        setTimeout( function() { setMessage("")},3000);
+        setMessage2("Campo obbligatorio");
+        setTimeout( function() { setMessage("")},3000);
+       
+        
+    } 
+    
+    if(email === "") {
+
+       setPassword(password);
+       setMessage("Campo obbligatorio");
+       setTimeout( function() { setMessage("")},3000);
+   }
+   
+ 
+
+   if(password === "") {
+
+       setEmail(email);
+       setMessage2("Campo obbligatorio");
+       setTimeout( function() { setMessage2("")},3000);
+     
+       
+   } 
+
+   if(email !== "" && password !== "") 
+   {
+   setEmail("");
+   setPassword("");
+   setMessage3("Messaggio inviato");
+   setTimeout( function() { setMessage3("")},3000);
+   }
+
+}
+
   return (
     <div className="accedi">
           <nav className="navbar navbar-dark bg-dark">
@@ -11,21 +63,24 @@ function Accedi() {
                 <button type="button" className="btn btn-danger text-light btn-info" ><Link className="link-light text-decoration-none" to="/">Indietro</Link></button>
              </div>
           </nav>
-              <h1 className="text-center text-danger">Accedi</h1>
+              <h1 className="text-center text-danger fs-1">Accedi</h1>
 
-            <form className="border border-info rounded bg-info w-75">
-                <div class="mb-3">
-                   <label for="email" className="form-label fs-4">Email address</label>
-                   <input type="email" className="form-control border-info w-75" id="email" aria-describedby="emailHelp" />
+            <form className="border border-info rounded bg-info w-50 mx-auto">
+                <div className="mb-3">
+                   <label htmlFor="email" className="form-label fs-4">Email</label>
+                   <input type="email" className="form-control border-info" id="email" value={email} onChange={ (e) => setEmail(e.target.value)} />
+                   <p className="card-text text-center fw-bolder text-danger">{message}</p>
   
                 </div>
 
-                <div class="mb-3">
-                   <label for="password" class="form-label fs-4" id="psw">Password</label>
-                   <input type="password" class="form-control border-info w-75" id="password" />
+                <div className="mb-3">
+                   <label htmlFor="password" className="form-label fs-4">Password</label>
+                   <input type="password" className="form-control border-info" id="password" value={password} onChange={ (e) => setPassword(e.target.value)} />
+                   <p className="card-text text-center fw-bolder text-danger">{message2}</p>
                 </div>
-
-                <button type="submit" class="btn btn-primary fs-4">Invia</button>
+                <br />
+                <button type="submit" onClick={handleSubmit} className="btn btn-primary fs-4">Invia</button>
+                <p className="card-text text-center fw-bolder text-success">{message3}</p>
                 
             </form>  
     </div>
