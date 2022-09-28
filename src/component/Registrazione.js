52f079a9361a73111e538f60c6a15f0d8d7cdfb3
giorgiocoppola <giorgiocoppola12@gmail.com>
2022-09-28 23:03:34 +0200

    Inserimento iniziale della pagina web Registrazione

---
+++ src/component/Registrazione.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+
+
+function Registrazione() {
+  return (
+    <div className="registrazione">
+         <div className="accedi">
+          <nav className="navbar navbar-dark bg-dark">
+             <div className="container-fluid">
+                <span className="navbar-brand mb-0 h1"><Link className="link-light text-decoration-none" to="/">Pizzeria Angolo Rosso</Link></span>
+             </div>
+          </nav>
+              <h1 className="text-center text-danger fs-1">Accedi</h1>
+      
+         </div>
+    </div>
+  )
+}
+
+export default Registrazione
