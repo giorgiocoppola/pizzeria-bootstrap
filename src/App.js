92eff523be585ab8c9e3a6df9bd570f7d4c0936d
giorgiocoppola <giorgiocoppola12@gmail.com>
2022-09-28 23:05:33 +0200

    Inserimento del route registrazione

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react'
 import { BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import Home from './component/Home'
 import Accedi from './component/Accedi'
+import Registrazione from './component/Registrazione'
 
 function App() {
   return <Router >
            <Routes>
             <Route path='/' element={<Home />} />
             <Route path='/accedi' element={<Accedi />} />
+            <Route path='/registrazione' element={<Registrazione />} />
            </Routes>
          </Router>  
 }
