b3acfa8199f5fe88d413283fba7678a4fa617c24
giorgiocoppola <giorgiocoppola12@gmail.com>
2022-09-29 12:12:03 +0200

    Creazione del Footer

---
+++ src/component/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className="footer bg-dark text-light text-center fs-5 pt-3 pb-4">
+
+             PIZZERIA ANGOLO ROSSO<br /> 
+           Via A. Manzoni, 25 - 74015 - Ancona<br />
+           email:pizzeriaangolorosso@gmail.com
+    </div>
+  )
+}
+
+export default Footer
